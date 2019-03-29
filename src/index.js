a53258264d5f0b4ca3272b052a9dd9f62047d6a4
Philipp Fromme <philippfromme@outlook.com>
2019-03-29 21:40:00 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -10,16 +10,128 @@ import Grains from './Grains';
 import DragAndDrop from './DragAndDrop';
 import AutoPlay from './AutoPlay';
 
+const PRESETS = [
+  {
+    name: 1,
+    url: './example1.mp3'
+  },
+  {
+    name: 2,
+    url: './example2.mp3'
+  },
+  {
+    name: 3,
+    url: './example3.mp3'
+  },
+  {
+    name: 4,
+    url: './example4.mp3'
+  }
+];
+
 const pillPlay = document.getElementById('pill-play'),
       pillLoading = document.getElementById('pill-loading'),
-      canvases = document.getElementById('canvases');
+      canvases = document.getElementById('canvases'),
+      presets = document.getElementById('presets');
+
+let autoPlay,
+    dragAndDrop,
+    granular;
+
+const AUDIO_BUFFER_CACHE = {};
+
+function cancel(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+async function loadUserData(data) {
+  autoPlay.stop();
+
+  pillPlay.textContent = 'Play';
+
+  pillLoading.classList.remove('hidden');
+  pillPlay.classList.add('inactive');
+  presets.classList.add('inactive');
+
+  const buttons = Array.from(document.querySelectorAll('#presets .preset'));
+
+  buttons.forEach(b => b.classList.add('pill-inverted'));
+
+  await granular.setBuffer(data);
+
+  pillLoading.classList.add('hidden');
+  pillPlay.classList.remove('inactive');
+  presets.classList.remove('inactive');
+}
+
+async function loadPreset({ name, url }) {
+  if (process.ENV === 'development') {
+    console.log(`load preset ${ name }`);
+  }
+
+  autoPlay.stop();
+
+  pillPlay.textContent = 'Play';
+
+  pillLoading.classList.remove('hidden');
+  pillPlay.classList.add('inactive');
+  presets.classList.add('inactive');
+
+  let data;
+
+  if (AUDIO_BUFFER_CACHE[ name ]) {
+
+    // AudioBuffer
+    data = AUDIO_BUFFER_CACHE[ name ];
+  } else {
+
+    // ArrayBuffer
+    data = await getData(url);
+  }
+
+  const audioBuffer = await granular.setBuffer(data);
+
+  AUDIO_BUFFER_CACHE[ name ] = audioBuffer;
+
+  pillLoading.classList.add('hidden');
+  pillPlay.classList.remove('inactive');
+  presets.classList.remove('inactive');
+}
+
+function createPresets(data, text) {
+  PRESETS.forEach((preset) => {
+    const { name } = preset;
+
+    const button = document.createElement('div');
+
+    button.classList.add('preset', 'pill', 'pill-inverted', 'pill-button');
+
+    button.textContent = name;
+
+    button.addEventListener('click', () => {
+      const buttons = Array.from(document.querySelectorAll('#presets .preset'));
+
+      buttons.forEach((b) => {
+        if (button === b) {
+          b.classList.remove('pill-inverted');
+        } else {
+          b.classList.add('pill-inverted');
+        }
+      });
+
+
+      loadPreset(preset);
+    });
+
+    presets.appendChild(button);
+  });
+}
 
 async function init() {
-  const data = await getData('./example.wav');
-
   const audioContext = p5.prototype.getAudioContext();
 
-  const granular = new Granular({
+  granular = new Granular({
     audioContext,
     envelope: {
       attack: 0,
@@ -50,29 +162,17 @@ async function init() {
 
   new Grains(granular);
 
-  const dragAndDrop = new DragAndDrop(canvases);
-
-  // dragAndDrop.on('dragOver', () => console.log('drag over'));
+  dragAndDrop = new DragAndDrop(canvases);
 
   dragAndDrop.on('fileRead', async ({ data }) => {
-    autoPlay.stop();
-
-    pillPlay.textContent = 'Play';
-
-    pillLoading.classList.remove('hidden');
-    pillPlay.classList.add('inactive');
-
-    await granular.setBuffer(data);
-
-    pillLoading.classList.add('hidden');
-    pillPlay.classList.remove('inactive');
+    loadUserData(data);
   });
 
   granular.on('bufferSet', ({ buffer }) => {
     waveform.draw(buffer);
   });
 
-  const autoPlay = new AutoPlay(granular);
+  autoPlay = new AutoPlay(granular);
 
   pillPlay.addEventListener('click', (event) => {
     event.preventDefault();
@@ -87,11 +187,6 @@ async function init() {
 
       pillPlay.textContent = 'Stop';
     }
-  });
-
-  pillPlay.addEventListener('mousedown', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
   });
 
   window.addEventListener('keydown', (key) => {
@@ -110,10 +205,22 @@ async function init() {
     }
   });
 
-  await granular.setBuffer(data);
+  createPresets();
 
-  pillLoading.classList.add('hidden');
-  pillPlay.classList.remove('inactive');
+  const buttons = Array.from(document.querySelectorAll('#presets .preset'));
+
+  buttons.concat(pillPlay).forEach(element => {
+    [
+      'click',
+      'mousedown'
+    ].forEach(event => {
+      element.addEventListener(event, cancel);
+    });
+  });
+
+  buttons[0].classList.remove('pill-inverted');
+
+  await loadPreset(PRESETS[0]);
 }
 
 init();
